4c0729960c0d22853576f9a7462558510ecefe65
Stella McEwen <mcewen.stella@gmail.com>
2019-05-22 20:24:00 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {} 
+  @@artist_count = {}
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre) 
+    @@count += 1 
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    @@artists << artist
+    @@genres << genre 
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.artists 
+    @@artists.uniq!
+  end
+  
+  def self.genres 
+    @@genres.uniq!
+  end
+  
+  def self.genre_count 
+    @@genre_count.each do |genre| 
+      if self.genre_count[genre]
+        self.genre_count[genre] += 1 
+      else 
+        self.genre_count[genre] = 1 
+      end
+    end
+   genre_count
+  end 
+  
+  def self.artist_count 
+    @@artist_count 
+  end
+end
