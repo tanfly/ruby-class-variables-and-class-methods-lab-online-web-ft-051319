94f9361798776394a01cf56838521dee0f9b0de9
Stella McEwen <mcewen.stella@gmail.com>
2019-05-22 20:27:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,13 @@ class Song
   end 
   
   def self.artist_count 
-    @@artist_count 
+    artist_count = {} 
+    if artist_count[artist]
+      artist_count[artist] +=1 
+    else 
+      artist_count[artist] = 1 
+    end 
+  end
+  artist_count
   end
 end
