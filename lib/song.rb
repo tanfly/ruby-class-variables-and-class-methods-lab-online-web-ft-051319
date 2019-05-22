0a12eb9bbdb0f9b0eddf1ae72d4906ab6b34b06c
Stella McEwen <mcewen.stella@gmail.com>
2019-05-22 20:26:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,14 +27,15 @@ class Song
   end
   
   def self.genre_count 
-    @@genre_count.each do |genre| 
-      if @@genre_count[genre]
-        @@genre_count[genre] += 1 
+    genre_count = {}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+        genre_count[genre] += 1 
       else 
-        @@genre_count[genre] = 1 
+        genre_count[genre] = 1 
       end
     end
-   @@genre_count
+   genre_count
   end 
   
   def self.artist_count 
